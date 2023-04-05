35f280c361c918c989627a56544e45a87dac9ee6
SHUBHAM SINGH <128099554+SonaBabulover1@users.noreply.github.com>
2023-04-05 23:20:31 +0530

    python_code_pratice

---
+++ assigenment_opr_pro.py
@@ -0,0 +1,15 @@
+# (=) assigenment operator uses 
+a =10
+b=a
+print(b) 
+# output - will come b = 10 it will be take the value of a 
+# (+ = ) assignment operator uses
+a = 10
+b = a 
+b +=10
+print(b)
+#(-=) assignment operator uses
+a=10 
+b =a
+b -=2
+print(b)  

3f907207243f35de37827b74e14fc3d8cf649117
SHUBHAM SINGH <128099554+SonaBabulover1@users.noreply.github.com>
2023-04-06 13:16:04 +0530

    code_pratice

---
+++ str_pro_1.py
@@ -0,0 +1,20 @@
+# write a string program and check 
+b = 'harry'
+print(b)
+print(type(b))
+# double quoted
+c = "shalu"
+print(c)
+print(type(c))
+# triple quoted 
+d = '''Shweta'''
+print(d)
+print(type(d))
+# single and double 
+e = "shalu'shweta"
+print(e)
+print(type(e))
+# single ,double and triple
+f = '''shalu"shweta'shuhbam'"'''
+print(f)
+print(type(f))
